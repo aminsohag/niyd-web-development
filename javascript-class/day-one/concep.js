0a15f572214d96217fa56a61aeb8a172d50b693a
aminsohag <aminsohag508@gmail.com>
2024-12-30 10:39:32 +0600

    javascript explore

---
+++ javascript-class/day-one/concep.js
@@ -0,0 +1,42 @@
+Javascript 
+==========
+
+1. Row Javascript ECMA-5 Standard
+        Javascript basic SyntaxError    
+        variable and datatype
+                 3 major rules 
+                        start var or let sign
+                        a-z, A-Z, , _, 
+
+        Operator
+        Statement 
+        Array
+        Object
+        Event & AudioListener
+        Input/Output
+        DOM/BOM 
+        Timing Method 
+2. Javascript Library 
+        jQuery
+3. Javascript Framework
+        Font -
+            React 
+            Angular
+            Vue
+        Backend -
+            Node.js - es6     
+4. Javascript Process 
+        AJAX             
+
+Before create anything first create specification
+
+Specification - class
+    Information - property    
+    Activity - method
+
+Real thing - object  
+ 
+ class
+ property 
+ method 
+ object
